d5aaf512a163b30a4f336c2b1f50ecc5c3497e5b
Sergio Rivas <sergiorivas@163.com>
2020-02-19 18:50:43 +0800

    update src file

--- dist/lib/language-wxss/transformer.js
+++ dist/lib/language-wxss/transformer.js
@@ -1,9 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 function styleTransformer(sourceCode, options) {
-    const css = sourceCode.replace(/([\d]+\s*)r(px|em)/g, (m, $1, $2) => {
+    const css = sourceCode.replace(/([\d]+)rpx/g, (m, $1) => {
         const newPx = $1.replace(/(^:)|(:$)/g, '');
-        return `${newPx * 0.5}${$2}`;
+        return `${newPx * 0.5}px`;
     });
     return css;
 }
--- src/language-wxss/transformer.ts
+++ src/language-wxss/transformer.ts
@@ -1,7 +1,8 @@
 export default function styleTransformer (sourceCode : string, options : any) {
-  const css = sourceCode.replace(/([\d]+)rpx/g, (m, $1) => {
+    const css = sourceCode.replace(/([\d]+\s*)r(px|em)/g, (m, $1, $2) => {
+
     const newPx = $1.replace(/(^:)|(:$)/g, '')
-    return `${newPx * 0.5}px`
+    return `${newPx * 0.5}${$2}`;
   })
   return css
 }
